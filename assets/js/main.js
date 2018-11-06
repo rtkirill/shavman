cfca3a7b8b76bd88d3b2e69b0abc0f634e82cd70
rtkirill <elpuffru@gmail.com>
2018-11-07 02:01:04 +0300

    Edit condition for contact to item. Add score. Add enemy and new animation for this, make reload game. Add sounds.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,31 @@ function main() {
     var cvs = document.getElementById("canvas");
     var ctx = cvs.getContext("2d");
 
+    //Load sounds
+    var bgSound = new Audio();
+    var dreamSound = new Audio();
+    var enemySound = new Audio();
+
+    bgSound.src = 'assets/sounds/bgSound.mp3';
+    dreamSound.src = 'assets/sounds/forDream.mp3';
+    enemySound.src = 'assets/sounds/forEnemy.mp3';
+
     //Load images
     var bgImg = new Image();
     var heroImg = new Image();
     var enemyImg = new Image();
-    var goalImg = new Image();
+    var dreamImg = new Image();
     var successImg = new Image();
     var failImg = new Image();
 
     bgImg.src = 'assets/img/BgImg.jpg';
     heroImg.src = 'assets/img/HeroImg.png';
     enemyImg.src = 'assets/img/enemyImg.png';
-    goalImg.src = 'assets/img/goalImg.png';
+    dreamImg.src = 'assets/img/dreamImg.png';
     successImg.src = 'assets/img/SuccessImg.png';
     failImg.src = 'assets/img/FailImg.png';
+
+
 
     var hero = {
         x: 50,
@@ -28,13 +39,10 @@ function main() {
         x: 100,
         y: 0
     };
-    var enemy = [];
-    enemy[0] = {
-        x: cvs.width - 100,
-        y: 0
-    };
-    var items = ["goal", "enemy"];
-    var itemImg;
+    var items = ["dream", "enemy"];
+    var randItem = "dream"
+    var speed = 4;
+    var score = 0;
 
     document.onkeydown = function(e) {
         if(e.code == "ArrowLeft" && hero.x >= 20)
@@ -45,46 +53,64 @@ function main() {
 
     //Draw items
     function draw() {
-       /* var randItem = items[Math.floor(Math.random() * items.length)];
-        console.log(randItem);*/
-
         //Restore
         ctx.clearRect(0, 0, cvs.width, cvs.height);
+        // ctx.globalAlpha = 1.0;
+
+        // bgSound.play();
 
         //Hero
         ctx.drawImage(bgImg,0,0);
         ctx.drawImage(heroImg,hero.x,hero.y);
 
         for(var i=0; i<goal.length; i++) {
-            ctx.drawImage(goalImg,goal[i].x,goal[i].y);
-            goal[i].y += 4;
+            randItem=="dream" ?
+                ctx.drawImage(dreamImg,goal[i].x,goal[i].y) : ctx.drawImage(enemyImg,goal[i].x,goal[i].y);
 
-            // console.log(goal[i].y);
+            goal[i].y += speed;
 
-            if(goal[i].y == 260) {
+            if(goal[i].y>=cvs.height && goal[i].y<cvs.height+speed) {
                 goal.push({
                     x: Math.floor(Math.random() * (cvs.width-70)),
                     y: 0
                 });
+
+                randItem = items[Math.floor(Math.random() * items.length)];
             }
 
-            if(hero.x <= goal[i].x && hero.x+heroImg.width >= goal[i].x+goalImg.width
+            if(hero.x <= goal[i].x+dreamImg.width
+                && hero.x+heroImg.width >= goal[i].x
                 && hero.y <= goal[i].y && goal[i].y < 500) {
-                ctx.drawImage(successImg,hero.x,hero.y);
+                if(randItem == "dream")
+                    ctx.drawImage(successImg,hero.x,hero.y);
+                else {
+                    if(goal[i].y>=hero.y && goal[i].y<hero.y+speed) {
+                        ctx.drawImage(failImg, hero.x, hero.y);
+                        // enemySound.play();
+                        score = 0;
+                        location.reload();
+                    }
+                }
+
+                if(goal[i].y>=hero.y && goal[i].y<hero.y+speed) {
+                    score++;
+                    // dreamSound.play();
+                    if (score > 0 && score % 5 == 0 && speed<8) {
+                        speed++;
+                    }
+                }
+
             }
 
+            ctx.fillStyle = "#fff";
+            ctx.font = "24px Verdana";
+            ctx.fillText("SCORE: " + score, cvs.width - 150, cvs.height - 20);
+
+            /*if() {
+                ctx.globalAlpha = 0.1;
+                ctx.drawImage(dreamImg,goal[i].x,goal[i].y);
+            }*/
         }
-
-
-
-        /*if(hero.x >= goal.x && hero.x <= goal.x+goal.width
-            && hero.x+hero.width <= goal.x && hero.x+hero.width >= goal.x) {
-            ctx.drawImage(successImg,hero.x,hero.y);
-        }*/
-
-
-        /*ctx.drawImage(goalImg,goalPosX,goalPosY);
-        ctx.drawImage(enemyImg,enemyPosX,enemyPosY);*/
 
         requestAnimationFrame(draw);
     }
